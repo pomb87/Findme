74948107b4331e9c0c4be95804033b4d9692ebff
pombdev <pombdev@gmail.com>
2019-11-10 17:26:36 +0100

    Initial commit

---
+++ app/src/main/java/com/pombdev/games/pb/findme/GameActivity.java
@@ -0,0 +1,6 @@
+package com.pombdev.games.pb.findme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/pombdev/games/pb/findme/MenuActivity.java
@@ -0,0 +1,32 @@
+package com.pombdev.games.pb.findme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton buttonPlay;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /** init buttons **/
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonPlay) {
+            //TODO
+            //transition from MenuActivity to GameActivity
+            startActivity(new Intent(MenuActivity.this, GameActivity.class));
+        }
+    }
+}
